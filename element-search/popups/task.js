60f67ca2211c520d8e79a1de15651dacd2af2c26
bel-lov <belou.lyuba@yandex.ru>
2022-05-29 18:10:58 +0300

    error correction

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,23 +1,26 @@
 const element = Array.from(document.querySelectorAll('.modal'));
-let elementSuccess = document.querySelector('#modal_success');
+let elementSuccess = document.getElementById('modal_success');
 let showClasses = document.querySelector('.show-success');
 //element.className += "modal_active"; //добавляет в начало окно
+let closing = document.querySelectorAll(".modal__close");//все элементы крестиков
 
-let closing = document.querySelector('.modal__close');
-
-function openModal () {
-    element[0].classList.add("modal_active");
+for (let i = 0; i < closing.length; i++) {
+    closing[i].onclick = closeModal;
 }
-openModal();
+
+element[0].classList.toggle("modal_active");
+
+// function openModal () {
+//     element[0].classList.add("modal_active");
+// }
+// openModal();
 
 function closeModal() {
     element.forEach((element)=> {element.classList.remove("modal_active")});
     // element.classList.remove("modal_active")
 }
 
-closing.onclick  = function(){
-    closeModal();
-}
 showClasses.onclick = function(){
-    elementSuccess.classList.add("modal_active"); 
+    element[0].classList.toggle("modal_active");
+    elementSuccess.classList.toggle("modal_active"); 
 }
