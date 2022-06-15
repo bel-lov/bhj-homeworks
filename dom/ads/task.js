16c9f741b81e988fd1799269513ddf1598665994
bel-lov <belou.lyuba@yandex.ru>
2022-06-15 23:08:04 +0300

    second commit ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,37 +1,14 @@
 const texts = document.querySelectorAll('.rotator__case');
 const rotator = document.querySelector('.rotator');
+const allDataAttribute = document.querySelectorAll('[data-speed]');
+let timeout = 1000;
 
-
-function addClass() {  
-    for (let i = 0; i < texts.length; i++) {
-        const text = texts[i];
-
-        const data = text.dataset.speed;
-        const color = text.dataset.color;
-
-        const activCase = rotator.querySelector('.rotator__case.rotator__case_active');
-
-        if (data === '2000') {
-            activCase.classList.remove('rotator__case_active');// удаляем класс у активного элемента
-            text.classList.add('rotator__case_active');
-            color.setAttribute('green'); 
-        }
-        else if(data === '1000'){
-            activCase.classList.remove('rotator__case_active');
-            text.classList.add('rotator__case_active');
-            //color.setAttribute('#000');
-        }
-        else if(data === '500'){
-            activCase.classList.remove('rotator__case_active');
-            text.classList.add('rotator__case_active');
-        }
-        else if(data === '200'){
-            activCase.classList.remove('rotator__case_active');
-            text.classList.add('rotator__case_active');
-        }
-    }
-}
-
-setInterval(() => {
-    addClass(texts);
-}, 1000);
+allDataAttribute.forEach(el=>{
+    timeout += Number(el.dataset.speed);
+    let activCase = rotator.querySelector('.rotator__case.rotator__case_active');
+    el.style.color = el.dataset.color;
+    setTimeout(() =>{
+        activCase.classList.remove('rotator__case_active');
+        el.classList.add('rotator__case_active');  
+    }, timeout);
+});
