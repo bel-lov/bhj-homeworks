51051e884f150d3a93b46e9d972d3f9f90975622
bel-lov <belou.lyuba@yandex.ru>
2022-06-16 19:38:56 +0300

    second commit ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,14 +1,19 @@
 const texts = document.querySelectorAll('.rotator__case');
 const rotator = document.querySelector('.rotator');
-const allDataAttribute = document.querySelectorAll('[data-speed]');
-let timeout = 1000;
+const allDataAttribute = Array.from(document.querySelectorAll('[data-speed]'));
+let timeout = 0;
+let count = allDataAttribute[0];
 
 allDataAttribute.forEach(el=>{
     timeout += Number(el.dataset.speed);
-    let activCase = rotator.querySelector('.rotator__case.rotator__case_active');
     el.style.color = el.dataset.color;
-    setTimeout(() =>{
+    if(count > allDataAttribute.length){
+        count = allDataAttribute[0];
+    }
+    
+    setInterval(() =>{
+        let activCase = rotator.querySelector('.rotator__case.rotator__case_active');
         activCase.classList.remove('rotator__case_active');
-        el.classList.add('rotator__case_active');  
+        el.classList.add('rotator__case_active');
     }, timeout);
 });
