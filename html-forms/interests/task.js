a3e4438e7e9ea41b75afb06a3a6e0f528d351aac
bel-lov <belou.lyuba@yandex.ru>
2022-06-28 12:38:33 +0300

    second commit interests

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,41 +1,15 @@
-const interestCheckElems = document.querySelectorAll('.interest__check');//li length 6
-const interestElems = document.querySelector('.interest');
-const interestsUlElems = document.querySelectorAll('.interests.interests_active');//ul length 2
-const ingredients = document.querySelectorAll('ul input');
 
-//  Логика у меня такая. 
-//  Нахожу input с типом checkbox по классу .interest__check. 
-//  Затем добавляю на него обработчик события в котором нахожу детей этого элемента и проставляю checked = true. 
-//  При нажатии на input, детей он находит, но не проставляет им галочки.
-//Подсккажите, пожалуйста, алгоритм действий.
-const checkbox = document.querySelector(".interest__check");
-checkbox.addEventListener("change", function (){
-let elems = interestElems.children;
-	console.log(elems);
-    elems.checked = true;
+addEventListener('change', e => {
+	const target = e.target
+
+	if ( target.classList.contains('interest__check') ) {
+		const interest = target.closest('.interest')
+		const interests = interest.querySelector('.interests')
+
+		if ( interests ) {
+			const checkboxElems = interests.querySelectorAll('.interest input[type=checkbox]')
+
+			Array.from(checkboxElems).forEach(checkbox => checkbox.checked = target.checked)
+		}
+	}
 })
-
-
-//const checkbox = document.querySelectorAll(".interest__check");
-// checkbox.forEach(el => {
-//     el.addEventListener("change", function () {
-//         console.log(el);
-//         if (el.checked) {
-
-//             if (el.children) {
-//                 let elems = el.children;
-//                 for (let elem of elems) {
-//                     console.log('checked');
-//                     elem.checked = true;
-//                 }
-//             }
-//             // interestsUlElems.forEach(a =>{
-//             // a.checked = true;
-//             // console.log('checked');
-//             // });
-//         } else {
-//             console.log("unchecked");
-//         }
-//     })
-// })
-
