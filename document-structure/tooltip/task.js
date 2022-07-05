148cec54b3cb5bfbf895e4cfe3cf8ce54828e8ef
bel-lov <belou.lyuba@yandex.ru>
2022-07-05 23:09:21 +0300

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,41 +1,49 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
-document.addEventListener('click', e =>{
+document.addEventListener('click', e => {
     if (e.target.classList.contains('has-tooltip') || e.target.closest('.has-tooltip')) {
         let target
-		if (e.target.classList.contains('has-tooltip')) {
-			target = e.target
-		}
-		else {
-			target = e.target.closest('.has-tooltip')
-		}
+        if (e.target.classList.contains('has-tooltip')) {
+            target = e.target
+        }
+        else {
+            target = e.target.closest('.has-tooltip')
+        }
         e.preventDefault()
-		tooltipShow(target)
+        tooltipShow(target)
     }
     else {
-		tooltipHide()
-	}
+        tooltipHide()
+    }
 })
 
-function tooltipShow(link){
-    if(!document.querySelector('.tooltip')){
+
+function tooltipShow(link) {
+    if (!document.querySelector('.tooltip')) {
         createTooltip(link);
     }
     const tooltip = document.querySelector('.tooltip');
 
-	tooltip.innerHTML = link.getAttribute('title');
-	tooltip.classList.add('tooltip_active');
-    tooltip.style.left = link.getBoundingClientRect().left + ( link.offsetWidth / 2 ) - ( tooltip.offsetWidth / 2 ) + 'px';
-    tooltip.style.top = link.getBoundingClientRect().top + window.scrollY - tooltip.clientHeight - 4 + 'px';
+    tooltip.innerHTML = link.getAttribute('title');
+    tooltip.classList.toggle('tooltip_active');
+    if (link.getBoundingClientRect().left == 0) {
+        tooltip.style.left = link.getBoundingClientRect().left + 'px';
+        tooltip.style.top = link.getBoundingClientRect().top + window.scrollY - tooltip.clientHeight - 4 + 'px';
+    }
+    else {
+        tooltip.style.left = link.getBoundingClientRect().left + (link.offsetWidth / 2) - (tooltip.offsetWidth / 2) + 'px';
+        tooltip.style.top = link.getBoundingClientRect().top + window.scrollY - tooltip.clientHeight - 4 + 'px';
+    }
+
 }
 
-function tooltipHide(){
+function tooltipHide() {
     const tooltip = document.querySelector('.tooltip');
-    if(tooltip){
+    if (tooltip) {
         tooltip.classList.remove('tooltip_active');
     }
 }
 
-function createTooltip(){
+function createTooltip() {
     const tooltip = document.createElement('div');
     console.log(tooltip);
     tooltip.classList.add('tooltip');
