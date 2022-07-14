1fc971445ea4225892cb19d6ebb6246420a6b994
bel-lov <belou.lyuba@yandex.ru>
2022-07-14 10:30:31 +0300

    second commit text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,7 @@
 const textArea = document.querySelector('#editor');//// Инициализировала и присвоила переменной элемент textarea
 
-textArea.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-        localStorage.setItem('key', textArea.value)
-    }
+textArea.addEventListener('keydown', () => {
+    localStorage.setItem('key', textArea.value)
 })
 textArea.value = localStorage.getItem("key")
 
