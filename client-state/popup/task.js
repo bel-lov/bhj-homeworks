123175a835cad6cc2bac35d9ef5f553821bcb995
bel-lov <belou.lyuba@yandex.ru>
2022-07-14 10:20:34 +0300

    second commit popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,15 +2,15 @@ const modal = document.querySelector('.modal');
 const close = document.querySelector('.modal__close.modal__close_times');
 
 document.addEventListener('DOMContentLoaded', () => {
-    const cookie = getCookie('dom');
-    if (cookie === '5') {
+    const cookie = getCookie('modal__close');
+    if (cookie === 'true') {
         modal.classList.remove('modal_active');
     }
     else {
         modal.classList.add('modal_active');
         close.addEventListener('click', () => {
             modal.classList.remove('modal_active');
-            setCookie('dom', '5');
+            setCookie('modal__close', 'true');
         })
     }
 })
@@ -21,19 +21,8 @@ function getCookie(name) {//читает значение по имени кук
         const cookie = pairs.find(p => p.startsWith(name + '='))
         return cookie.substring(name.length + 1)
     }
-    return null;
+    // return null;
 }
-
-function eraseCookie(name) {   //удаляет куки
-    document.cookie = name+'=; Max-Age=-99999999;';  
-}
-
-
-function setCookie(name, value){ //устанавливает куки
+function setCookie(name, value) { //устанавливает куки
     document.cookie = name + '=' + encodeURIComponent(value)//зписывем новую куку в документ куков, при этом значение кодируем
 }
-
-
-document.cookie = 'age =; Expires=Thu, 01 Jan 1970 00:00:00 GMT'
-
-console.log(document.cookie)
