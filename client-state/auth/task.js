91d729f70bb9cdc037d68492989626277aad077d
bel-lov <belou.lyuba@yandex.ru>
2022-11-05 17:53:37 +0300

    finish commit

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,35 +1,32 @@
 const form = document.querySelector('#signin__form');
-const button =document.querySelector('#signin__btn');
+const button = document.querySelector('#signin__btn');
 const welcomeId = document.querySelector('#welcome');
 const welcomActiv = document.querySelector('.welcome');
+const signin = document.querySelector('#signin');
 
-button.addEventListener('click',function () {
+button.addEventListener('click', function () {
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         const formData = new FormData(form);
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', () =>{
-        
-        if(xhr.readyState === xhr.DONE){
-            response_obj = JSON.parse(xhr.responseText);
-            if(response_obj.success){
+        xhr.responseType = "json";
+        xhr.addEventListener('load', () => {
+            // const response_obj = JSON.parse(xhr.responseText);
+            const response_obj = xhr.response;
+            if (response_obj.success) {
                 localStorage.setItem('user_id', response_obj.user_id);
                 welcomActiv.textContent = welcomActiv.textContent + localStorage.getItem('user_id');
                 welcomActiv.classList.add('welcome_active');
+                signin.classList.remove('signin_active');
+                form.reset();
             }
-            else{
-                alert("Неверный логин/пароль"); 
+            else {
+                alert("Неверный логин/пароль");
             }
-        }
-        else {
-            alert("Ошибка " + xhr.status);
-        }
         })
         xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
         xhr.send(formData);
-        // xhr.onload = () => alert(xhr.status);
     });
 })
-
 
 
